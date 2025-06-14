4f7cca196cc85532104fd39a02211cd5f6225ee7
cristian <cristian-anes@hotmail.es>
2025-06-14 22:21:15 +0200

    nuevos csambios

--- demo/src/main/java/com/mialquiler/demo/config/SecurityConfig.java
+++ demo/src/main/java/com/mialquiler/demo/config/SecurityConfig.java
@@ -17,13 +17,10 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @EnableMethodSecurity
 public class SecurityConfig {
 
-<<<<<<< HEAD
     // Inyectar el UserDetailsService personalizado
     @Autowired
     private DatabaseUserDetailsService userDetailsService;
 
-=======
->>>>>>> 56583d66547f93991d0d6e8bb1399da0570643a9
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
